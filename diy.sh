0aba768fce2bc830b27a713705e8eb755b033cad
liuleisail <30498867+liuleisail@users.noreply.github.com>
2020-04-24 07:20:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.2.2/192.168.2.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.3/g' package/base-files/files/bin/config_generate
 sed -i '5s/#//' feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
